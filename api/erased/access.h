f752e837d3963b62f438a34f1385f21a3c267fcc
krustenbrot <public@holad.de>
2016-04-09 09:53:25 +0200

    Update access.h
    Code review access.h

--- api/erased/access.h
+++ api/erased/access.h
@@ -16,6 +16,8 @@ namespace Erased {
 //-------------------------------------------------------------------------------------------------
 // access modifiers for base classes, fields and methods
 //-------------------------------------------------------------------------------------------------
+
+// HAD: Du kommentierst durchgängig viel Syntax, die aus dem Code eigentlich völlig selbstverständlich wird. Den obbigen Kommentar kann man theoretisch komplett weglassen.
 enum Access {
     kPrivate = 0,
     kProtected,
